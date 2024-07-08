3e2aeec8c2dbbeb15947b18ed45f73db0112341a
m4tt18 <mateus.s18@hotmail.com>
2024-07-08 17:56:28 -0300

    First Commit - Criação do projeto.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a sua senha: ");
+        String senha = scanner.next();
+        int tentativas = 1;
+
+        //Para alterar a senha "correta",
+        //basta mudar na linha abaixo o termo "INSIRASUASENHAAQUI" para a senha de sua escolha.
+        while(!senha.equals("INSIRASUASENHAAQUI") && tentativas <= 2) {
+            System.out.println(
+                    "Senha incorreta! Tente novamente. (Tentativas restantes: " + (3 - tentativas));
+            tentativas++;
+            senha = scanner.next();
+        }
+
+        if(senha.equals("INSIRASUASENHAAQUI")) {
+            System.out.println("Acesso permitido!");
+        } else {
+            System.out.println("Senha incorreta, acesso Bloqueado! (Limite de tentativas excedido)");
         }
     }
 }
